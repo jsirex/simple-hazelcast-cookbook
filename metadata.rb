2690a91bbf34543c3f93fe6f1da4976c94291c28
Yauhen Artsiukhou <jsirex@gmail.com>
2015-06-03 14:00:52 +0300

    Bump version: v0.2.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs only hazelcast'
 long_description 'Installs only hazelcast'
 issues_url       'https://github.com/jsirex/simple-hazelcast-cookbook/issues'
 source_url       'https://github.com/jsirex/simple-hazelcast-cookbook'
-version          '0.1.0'
+version          '0.2.0'
 
 
 depends 'ark'
